3648b69f6d1ece7a9c1ebad891975faf1497faac
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-01-25 03:32:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,40 @@
-var imageDiv = document.getElementById("image")
-var fullname = document.getElementById("fullName")
-var ageBtn = document.getElementById("ageBtn")
-var phoneBtn = document.getElementById("phoneBtn")
-var emailBtn = document.getElementById("emailBtn")
-var newUser = document.getElementById("getUser")
-var heading = document.getElementsByTagName("h")[0]
-var update = document.getElementById("update")
-
-var apiLink = "https://randomuser.me/api/"
-
-var userDATA;
-
-async function fetchingData() {
-    try {
-        var apiData = await fetch(apiLink)
-        var apiResponse = await apiData.json()
-        userDATA = apiResponse
-        insert(userDATA)
-    }
-    catch (error) {
-        console.log("Unable to fetch data" + error);
-    }
+async function fetchdata() {
+  const url = "https://randomuser.me/api/";
+  const data = await fetch(url);
+  const response = await data.json();
+  // console.log(response);
+  return response.results[0];
 }
+const showuser = () => {
+  fetchdata().then((data) => {
+    console.log(data.name.first, data.name.last);
+    console.log(data.picture.large);
+    document.getElementById(
+      "img-box"
+    ).innerHTML = `<img src="${data.picture.large}">`;
+    document.getElementById(
+      "username"
+    ).innerHTML = `${data.name.first} ${data.name.last}`;
+    const ageButton = document.getElementById("age");
+    ageButton.addEventListener("click", () => {
+      console.log(data.dob.age);
+      document.getElementById("output").innerHTML = `<h1>${data.dob.age}<h1>`;
+    });
+    const EmailButton = document.getElementById("email");
+    EmailButton.addEventListener("click", () => {
+      console.log(data.email);
+      document.getElementById("output").innerHTML = `<h1>${data.email}<h1>`;
+    });
+    const PhoneButton = document.getElementById("phone");
+    PhoneButton.addEventListener("click", () => {
+      console.log(data.phone);
+      document.getElementById("output").innerHTML = `<h1>${data.phone}<h1>`;
+    });
+  });
+};
 
-
-// fetchingData()
-document.addEventListener('DOMContentLoaded',fetchingData)
-
-function insert(dataOfuser) {
-
-    var image = document.createElement("img")
-    const source = dataOfuser.results; // this is an array
-    const source1 = source[0].picture.large // source[0] is the first element of an array, first element is itself an object
-    image.src = source1
-    imageDiv.append(image)
-    const readFirstName = source[0].name.first
-    const readLastName = source[0].name.last
-    fullname.textContent = readFirstName + " " + readLastName
-	
-function showAge(){
-    const source = userDATA.results;
-    const readAge = source[0].dob.age
-    heading.textContent = "Additional Information"
-    update.textContent = readAge
-}
-
-ageBtn.addEventListener('click', showAge)
-
-
-function showEmail(){
-    const source = userDATA.results[0].email;
-    heading.textContent = "Additional Information"
-    update.textContent = source
-}
-
-emailBtn.addEventListener('click', showEmail)
-
-
-function showPhone(){
-    const source = userDATA.results[0];
-    const readAge = source.phone
-    heading.textContent = "Additional Information"
-    update.textContent = readAge
-}
-
-phoneBtn.addEventListener('click', showPhone)
-}
-
-
-
-
-
-
-newUser.addEventListener('click', function(){
-    location.reload()
-})
+showuser();
+const newUser =document.getElementById("getUser");
+newUser.addEventListener("click", () => {
+  showuser();
+});
